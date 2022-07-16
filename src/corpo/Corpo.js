57981bdb0c3ae2d3f5057047083c513c5f071bba
Vinicius <vianaxd99@gmail.com>
2022-07-16 12:30:52 -0300

    topo componentizado

---
+++ src/corpo/Corpo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Esquerda from './esquerda/Esquerda';
+import Sidebar from './sidebar/Sidebar';
+
+export default function Corpo() {
+    return (
+        <div class="corpo">
+            <Esquerda />
+            <Sidebar />
+        </div>
+    )
+}
